7dbd41d79a1c98577ca031d4ed65ddc309e676f4
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-02-27 15:25:39 +0700

    multipart

--- app/src/main/java/com/example/iampamungkas/cameranyoba/interfaces/SeleKopiAPI.java
+++ app/src/main/java/com/example/iampamungkas/cameranyoba/interfaces/SeleKopiAPI.java
@@ -5,13 +5,16 @@ import com.example.iampamungkas.cameranyoba.models.ImageResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 /**
  * Created by iampamungkas on 2/26/17.
  */
 
 public interface SeleKopiAPI {
+    @Multipart
     @POST("KopiSelection/")
-    Call<ImageResponse> getImage(@Body ImageRequest imageRequest);
+    Call<ImageResponse> getImage(@Part ImageRequest imageRequest);
 }
